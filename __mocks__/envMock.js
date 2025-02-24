e08868ac98045e4f700435fc71491a1d5f023c29
Benjamin Jacob <8972@holbertonstudents.com>
2025-02-24 09:02:40 +0100

    add testing and CI configuration

---
+++ __mocks__/envMock.js
@@ -0,0 +1,8 @@
+module.exports = {
+  FIREBASE_API_KEY: 'mock-api-key',
+  FIREBASE_AUTH_DOMAIN: 'mock-auth-domain',
+  FIREBASE_PROJECT_ID: 'mock-project-id',
+  FIREBASE_STORAGE_BUCKET: 'mock-storage-bucket',
+  FIREBASE_MESSAGING_SENDER_ID: 'mock-sender-id',
+  FIREBASE_APP_ID: 'mock-app-id',
+};
