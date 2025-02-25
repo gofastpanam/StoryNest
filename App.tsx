a4ad2748fbdab5754fcf200897a84b1018ce88ba
Benjamin Jacob <8972@holbertonstudents.com>
2025-02-25 08:22:37 +0100

    add React Navigation setup, create RootStackParamList , Stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenue sur StoryNest!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{
+            title: 'StoryNest'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
